2ac40693f92b16b6b8fca43cf02dbf48639d3af5
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-09-09 21:25:06 -0400

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'error.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
+  }
+};
+
+export default config;
