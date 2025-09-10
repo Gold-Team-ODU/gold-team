774576f92b1849f2bb33736a7d4bd9348bd5a300
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-09-09 21:36:57 -0400

    Update build output path and adapter options
    Changed the build artifact path in the deploy workflow to 'build/' for consistency. Simplified Svelte adapter options by removing 'pages', 'assets', 'precompress', and 'strict' properties.

--- svelte.config.js
+++ svelte.config.js
@@ -7,11 +7,7 @@ const config = {
 
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
       fallback: 'error.html',
-      precompress: false,
-      strict: true
     }),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
