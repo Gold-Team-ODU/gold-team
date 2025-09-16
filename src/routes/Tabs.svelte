33fc2562c262bdf0f94ea6b5e64e28a5d6e9d3f9
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-09-16 19:14:11 -0400

    Make tab bar fully rounded and animate gradient
    Changed tab bar and shadow elements from rounded-3xl to rounded-full for a fully circular appearance. Added a CSS animation to rotate the gradient background for a dynamic visual effect.

--- src/routes/Tabs.svelte
+++ src/routes/Tabs.svelte
@@ -2,14 +2,14 @@
   <div class="relative">
     <!-- Gradient shadow thanks to https://ibelick.com/blog/create-gradient-shadows-tailwind-css -->
     <div
-      class="absolute -inset-1.5 preset-gradient opacity-75 blur rounded-3xl"
+      class="absolute -inset-1.5 preset-gradient opacity-75 blur rounded-full"
     ></div>
     <!-- White shine -->
     <div
-      class="absolute -inset-0 bg-surface-50 opacity-75 blur-[3px] rounded-3xl"
+      class="absolute -inset-0 bg-surface-50 opacity-75 blur-[3px] rounded-full"
     ></div>
     <!-- Main tab bar -->
-    <div class="relative preset-tonal-surface rounded-3xl">
+    <div class="relative preset-tonal-surface rounded-full">
       <!-- Center icons & cut out text (https://stackoverflow.com/a/71206137/17004103) -->
       <div
         class="place-self-center bg-gradient-to-r from-primary-contrast-200-800 to-tertiary-contrast-200-800 text-transparent bg-clip-text"
@@ -68,11 +68,27 @@
 <style>
   .preset-gradient {
     background-image: linear-gradient(
-      45deg,
+      var(--rotate),
       var(--color-primary-500),
       var(--color-tertiary-500)
     );
     color: var(--color-primary-contrast-500);
     backdrop-filter: blur(16px);
+    animation: rotate 2s infinite alternate ease-in-out;
+  }
+
+  @property --rotate {
+    syntax: "<angle>";
+    initial-value: 135deg;
+    inherits: false;
+  }
+
+  @keyframes rotate {
+    from {
+      --rotate: 20deg;
+    }
+    to {
+      --rotate: 160deg;
+    }
   }
 </style>
